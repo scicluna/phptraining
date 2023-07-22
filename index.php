76e3d07f7671cb9c2c2f82077c5a07f645e88be3
scicluna <88948700+scicluna@users.noreply.github.com>
2023-07-22 10:11:46 -0400

    tailwind online

--- index.php
+++ index.php
@@ -9,22 +9,12 @@
 
 <body>
     <?php
-    define("GREETING", "Hello Boys!");
-    define("BOYS", ["Bob", "Ricky", "BIG BOY"]);
-    $text = "Yo wassup";
-    $number = "52";
-    echo min([2, 2, 3, 4, 5]);
-    echo GREETING;
-    foreach (BOYS as $boy) {
-        echo $boy;
-    }
-    $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+    $cars = [["Volvo", 26, 17], ["BMW", 15, 2]];
 
-    foreach ($age as $x => $val) {
-        echo "$x = $val<br>";
+    for ($i = 0; $i < count($cars); $i++) {
+        echo $cars["Volvo"];
     }
-    echo str_replace("wassup", "bitch", $text);
-    echo array_keys($age);
+
 
     ?>
 </body>
